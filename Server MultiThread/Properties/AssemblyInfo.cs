7dd927b5ff9a7c37ecdc9fa164492dab914b2dbb
Luis Adrian Almaguer de la Garza <imaximuz@gmail.com>
2016-08-26 22:09:41 -0500

    He cambiado el nombre de "Server multithread" a solo "Server"

--- Server MultiThread/Properties/AssemblyInfo.cs
+++ Server MultiThread/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Server MultiThread")]
+[assembly: AssemblyTitle("Server")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Server MultiThread")]
+[assembly: AssemblyProduct("Server")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
